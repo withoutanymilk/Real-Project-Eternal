85b40d27938b70212234d0881c9c504f261a68a2
VoodooDoctor00 <Jsnav2000@yahoo.com>
2022-04-03 19:21:57 -0700

    Health bars for enemies
    Adds health bars to the enemies in Level 1.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float speed;
     private Transform playerPos;
     private PlayerHealth player;
+	public float maxHealth;
     public float health;
     public GameObject deathEffect;
     public int amount;
     public int damage;
     GameController cont;
 
+	public Image healthImage;
+	
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -35,6 +39,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		health = maxHealth;
 
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
         playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -47,7 +52,8 @@ public class Enemy : MonoBehaviour
     {
 
         transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
-
+		
+		healthImage.fillAmount = health / maxHealth;
     }
 
     void OnTriggerEnter2D(Collider2D enemy)
