75820cc38b6938bb88facd4064649097621909bc
withoutanymilk <squicky825@gmail.com>
2022-05-05 21:11:32 -0700

    Speed boost and Update - Hot fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,27 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("player speed and movement")]
-    public float moveSpeed = 5f;
+    public float moveSpeed = 6f;
     public Rigidbody2D rb;
     public Camera cam;
     private Vector2 moveInput;
     private Vector2 mousePos;
     public Animator animator;
 
+    public float boostTimer;
+    public bool boosting;
 
     // Update is called once per frame
     void Start()
     {
-
+        boostTimer = 0;
+        boosting = false;
     }
 
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
-              
+
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Speed", moveInput.x);
@@ -31,6 +34,27 @@ public class PlayerMovement : MonoBehaviour
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
+        if (boosting)
+        {
+            boostTimer -= Time.deltaTime;
+            if (boostTimer < 0)
+            {
+                moveSpeed = 6f;
+                boostTimer = 0;
+                boosting = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PowerupSpeed"))        //Triggers if the player collides with object with tag "PowerupSpeed", they gain 1.5 speed multiplier.
+        {
+            boosting = true;
+            moveSpeed = 10f;
+            boostTimer = 7;
+            Destroy(collision.gameObject);
+        }
     }
 
     void FixedUpdate()
@@ -41,6 +65,6 @@ public class PlayerMovement : MonoBehaviour
 
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 2;
 
-       rb.rotation = angle;
+        rb.rotation = angle;
     }
 }
